c1bf5ac844fd591d749cc96b2d0fe2d23aa06896
FawazHutomiA <hutomifawaza@gmail.com>
2023-10-04 10:12:32 +0700

    Add: Initiate Project

---
+++ components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-red-500 sticky top-0 h-20">
+        <h1>Ini Header</h1>
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+
+import TheHeader from '../components/TheHeader.vue';
+<template>
+    <div class="h-screen flex flex-col justify-between">
+        <div>
+            <TheHeader />
+            <slot />
+        </div>
+        <div class="bg-red-500">
+            <h1>Footer</h1>
+        </div>
+    </div>
+</template>
